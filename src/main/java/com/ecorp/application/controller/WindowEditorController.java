b9fa7c0d54a51436dd896cfd97a1baf5c81cea82
LeviathanZC <leviathan.zercomp@gmail.com>
2021-05-24 04:57:46 +0300

    Create stub controllers for scenes. Link classes with fxml files.

---
+++ src/main/java/com/ecorp/application/controller/WindowEditorController.java
@@ -0,0 +1,10 @@
+package com.ecorp.application.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WindowEditorController {
+
+    private static final Logger log = LoggerFactory.getLogger(WindowEditorController.class);
+
+}
---
+++ src/main/java/com/ecorp/application/controller/WindowJournalController.java
@@ -0,0 +1,10 @@
+package com.ecorp.application.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WindowJournalController {
+
+    private static final Logger log = LoggerFactory.getLogger(WindowJournalController.class);
+
+}
